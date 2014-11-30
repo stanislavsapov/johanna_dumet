127035c149ee4eb0a94991926b4921f7cca4ec25
2000donUt5 <s2stas@gmail.com>
2014-11-30 16:33:42 +0100

    Create extenal JS for index-gallery| Responsive design for width 360

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-function onHover()
-{
-    $("#index_about").attr('src', 'img/about.gif');
+/*var atag =  document.getElementById('index_about');
+
+atag.onmouseover = function() {
+    document.getElementById('index_about_img').src = 'img/about.gif';
 }
 
-function offHover()
-{
-    $("#indexgal").attr('src', 'images/about.png');
+atag.onmouseout = function() {
+    document.getElementById('index_about_img').src = 'img/about_index.gif';
+}
+*/
+
+function Roll (id,path){
+	document.getElementById(id).src = path;
 }
