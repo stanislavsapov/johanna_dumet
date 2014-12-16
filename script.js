5a42cfb705bea92bb489348c29b931ca493c40e7
2000donUt5 <s2stas@gmail.com>
2014-12-16 12:37:45 +0100

    resize pictn, navbar is centred, contact

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-/*var atag =  document.getElementById('index_about');
-
-atag.onmouseover = function() {
-    document.getElementById('index_about_img').src = 'img/about.gif';
-}
-
-atag.onmouseout = function() {
-    document.getElementById('index_about_img').src = 'img/about_index.gif';
-}
-*/
-
 function Roll (id,path){
-	document.getElementById(id).src = path;
+    document.getElementById(id).src = path;
 }
